d9895e92b620896d906b574177b01ceb3d3b848a
AsHMagic <84615219+AsHMagic@users.noreply.github.com>
2024-02-22 00:20:07 +0900

    Update chzzk-discord-presence-1.0.0.js

--- chzzk-discord-presence-1.0.0.js
+++ chzzk-discord-presence-1.0.0.js
@@ -18,26 +18,28 @@ async function activity(url, channel_id) {
             const data = await response.json();
             if (data && data.content) {
                 const { status, liveTitle, liveCategoryValue, concurrentUserCount, channel } = data.content;
-                const category = liveCategoryValue || '기타';
-                const user_count = concurrentUserCount || 'None';
-                const channel_name = channel ? channel.channelName || 'None' : 'None';
-                const channel_image = channel ? (channel.channelImageUrl || "https://ssl.pstatic.net/cmstatic/nng/img/img_anonymous_square_gray_opacity2x.png?type=f120_120_na") : "https://ssl.pstatic.net/cmstatic/nng/img/img_anonymous_square_gray_opacity2x.png?type=f120_120_na";
-                const preview_image = (data.content.liveImageUrl || '').replace('{type}', '1080');
-                const user_count_2 = user_count.toLocaleString();
-                if (RPC) {
-                    RPC.setActivity({
-                        details: liveTitle,
-                        state: `${category} 하는 중`,
-                        largeImageKey: preview_image,
-                        largeImageText: `${channel_name}`,
-                        smallImageKey: 'https://ssl.pstatic.net/static/nng/glive/icon/favicon.png',
-                        buttons: [
-                            {
-                                label: '보기',
-                                url: `https://chzzk.naver.com/live/${channel_id}`
-                            }
-                        ]
-                    });
+                if (status === 'OPEN') {
+                    const category = liveCategoryValue || '기타';
+                    const user_count = concurrentUserCount || 'None';
+                    const channel_name = channel ? channel.channelName || 'None' : 'None';
+                    const channel_image = channel ? (channel.channelImageUrl || "https://ssl.pstatic.net/cmstatic/nng/img/img_anonymous_square_gray_opacity2x.png?type=f120_120_na") : "https://ssl.pstatic.net/cmstatic/nng/img/img_anonymous_square_gray_opacity2x.png?type=f120_120_na";
+                    const preview_image = (data.content.liveImageUrl || '').replace('{type}', '1080');
+                    const user_count_2 = user_count.toLocaleString();
+                    if (RPC) {
+                        RPC.setActivity({
+                            details: liveTitle,
+                            state: `${category} 하는 중`,
+                            largeImageKey: preview_image,
+                            largeImageText: `${channel_name}`,
+                            smallImageKey: 'https://ssl.pstatic.net/static/nng/glive/icon/favicon.png',
+                            buttons: [
+                                {
+                                    label: '보기',
+                                    url: `https://chzzk.naver.com/live/${channel_id}`
+                                }
+                            ]
+                        });
+                    }
                 }
             }
         } else {
